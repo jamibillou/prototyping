536c8073ca2b1cb5afe0081798718f4bba164a2a
Franck Sabattier <franck.sabattier@gmail.com>
2012-06-27 00:02:02 +0200

    Form submit

--- app/controllers/shareable_controller.rb
+++ app/controllers/shareable_controller.rb
@@ -1,9 +1,14 @@
 class ShareableController < ApplicationController
   
   before_filter :set_titles
+  before_filter :candidate_signup, :only => [:candidate_profile_public]
   
   private
   
+  	def candidate_signup
+  		session[:candidate_full_name] = params[:candidate_full_name]
+  	end
+
     def set_titles
       @page_title = 'Prototypes'
       @page_subtitle = 'shareable profile'
