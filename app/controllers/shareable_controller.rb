95edc6d7d3b879f9d39400b00e2f9de368569095
Franck Sabattier <franck.sabattier@gmail.com>
2012-06-29 19:47:40 +0200

    Create default candidate session values

--- app/controllers/shareable_controller.rb
+++ app/controllers/shareable_controller.rb
@@ -7,11 +7,31 @@ class ShareableController < ApplicationController
   private
   
   	def candidate_signup
-      session[:candidate] = params[:candidate] if params[:candidate]
+      if params[:candidate]
+        if params[:candidate][:full_name].empty?
+          session[:candidate] = default_candidate
+        else
+          session[:candidate] = params[:candidate]
+          session[:candidate][:pic] = 'default_user.jpg'
+        end
+      end
     end
 
     def set_titles
       @page_title = 'Prototypes'
       @page_subtitle = 'shareable profile'
+    end
+
+    def default_candidate
+      return { :pic => 'wansa.jpg', :full_name => 'Wansa Sooksomwat', :city => 'Amsterdam', :country => 'Netherlands', 
+               :role => 'Retail Sales', :company => 'Esprit', :experience => '1', 
+               :education => 'HBO Bachelor in Communication & Media', 
+               :pro_skill_1 => { :name => 'External communication', :level => 'advanced'},
+               :pro_skill_2 => { :name => 'Event planning', :level => 'advanced'},
+               :pro_skill_3 => { :name => 'Writing', :level => 'intermediate'},
+               :inter_skill_1 => { :name => 'Good team mate', :level => 'expert'},
+               :inter_skill_2 => { :name => 'Attention to details', :level => 'intermediate'},
+               :inter_skill_3 => { :name => 'Organization', :level => 'intermediate'},
+               :catchphrase => "Communications isn't what a company says, it's everything else it does." }
     end
 end
